841af2c4ede9f392a5da14f9ebd800b4bfdd7c53
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-10 14:36:21 -0500

    import api

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # user access this fie to run project
-import apifile
+import api
 
 if __name__ == "__main__":
-    apifile.app.run(host="0.0.0.0", port=5003, debug=True)
+    api.app.run(host="0.0.0.0", port=5003, debug=True)
