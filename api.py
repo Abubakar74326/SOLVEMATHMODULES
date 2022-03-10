67473720ab14bb1ad505d4d28b2b7ad7bfd6ce9b
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-10 14:35:35 -0500

    import modulefunction

--- api.py
+++ api.py
@@ -1,20 +1,28 @@
 # from typing import Optional
 # from fastapi import FastAPI
 
+#from flask import Flask, request
+#import Flask, redirect, url_for, request
+
 from flask import Flask
-import allfunctions
+import modulefunction
 
 app = Flask(__name__)
 
 
-@app.get("/")
+'''@app.route('/read_root' ,methods = ['POST'])
 def read_root():
-    return {"Hello": "World"}
+    data = {
+            "NAME emoployee" ,request.form['NAME'] ,
+            "company", request.form['revolve'] ,
+            "designation", request.form['engineer']
+               }
+    return {data}'''
 
 
 @app.route("/function/<int:num>/") #math/
 def read_item(num: int): #math_func
-    object=allfunctions.Mathematics(int(num))
+    object=modulefunction.Mathematics(int(num))
     results={"number": int(num),
              "factorial": object.factorial(),
              "square": object.square(),
