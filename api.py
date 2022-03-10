8abb484c6b0571ed9ff7756ec92507dec193e024
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-10 15:47:27 -0500

    running form except data

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 # from typing import Optional
 # from fastapi import FastAPI
 
-#from flask import Flask, request
-#import Flask, redirect, url_for, request
+'''from flask import Flask, request
+#import Flask, redirect, url_for, request'''
 
 from flask import Flask
 import modulefunction
@@ -10,18 +10,23 @@ import modulefunction
 app = Flask(__name__)
 
 
-'''@app.route('/read_root' ,methods = ['POST'])
+@app.get("/")
+def read_root():
+    return {"MATH_PROJECT"}
+
+
+'''@app.route("/read_root/", methods=['POST'])
 def read_root():
     data = {
-            "NAME emoployee" ,request.form['NAME'] ,
-            "company", request.form['revolve'] ,
-            "designation", request.form['engineer']
+            "name emoployee", request.form['NAME'] ,
+            "employee company", request.form['COMPANY'] ,
+            "engineer", request.form['ENGINEER']
                }
     return {data}'''
 
 
-@app.route("/function/<int:num>/") #math/
-def read_item(num: int): #math_func
+@app.route("/math/<int:num>/") #math/
+def math_func(num: int): #math_func
     object=modulefunction.Mathematics(int(num))
     results={"number": int(num),
              "factorial": object.factorial(),
