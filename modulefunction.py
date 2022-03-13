72f1ed4ca7536b298a1c8f2ce4ae0f30acb243df
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-13 04:37:33 -0400

    Docstring is added in this file

--- modulefunction.py
+++ modulefunction.py
@@ -1,9 +1,13 @@
+# Below code return the factorial,square,cube,square root of the number.
+
 class Mathematics:
+    """this is main class whose name is mathematics"""
     def __init__(self, num):
         self.num = num
         self.num_copy = num
 
     def factorial(self):
+        """this factorial function return the factorial of a number """
         if self.num_copy != 0:
             self.num_copy -= 1
         if self.num_copy == 0:
@@ -12,10 +16,22 @@ class Mathematics:
             return (self.num_copy + 1) * self.factorial()
 
     def square(self):
+        """
+        this function return the square of
+        number that is enter in URL by user
+         """
         return self.num * self.num
 
     def cube(self):
+                """
+               this function return the CUBE of
+               number that is enter in URL by user by
+               multiply the number itself three times
+                """
         return self.num * self.num * self.num
 
     def square_root(self):
+        """
+         this function return the square of a number
+                     """
         return self.num ** 0.5
