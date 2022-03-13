c34b474839fa25bff98362aff7cf9d3b724d39f4
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-13 05:08:52 -0400

    run this file to execute the project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # user access this fie to run project
 import api
+"""modulefunction file is import in api file and api import in main file to run the project """
 if __name__ == "__main__":
     api.app.run(host="0.0.0.0", port=5004, debug=True)
