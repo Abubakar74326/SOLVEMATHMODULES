ca22a4d2751eac2f957fc5dc2d4b06d21f07c452
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-12 23:29:41 -0500

    little change in code

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"MATH_PROJECT}
 '''@app.route("/read/", methods=['POST'])
 def read_root():
     data = {
