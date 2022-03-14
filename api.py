35d2161e9d6360532de6eb82471b1dca5aba3900
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-13 23:48:13 -0400

    api file

--- api.py
+++ api.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 @app.route("/mathoperation/<int:num>/")
 def math_func(num: int):
     object=modulefunction.Mathematics(int(num))
-    results={"number": int(num),
-             "factorial": object.factorial(),
+    results={"YOUR ENTERED NUMBER": int(num),
+             "factoriaL": object.factorial(),
              "square": object.square(),
              "cube": object.cube(),
              }
