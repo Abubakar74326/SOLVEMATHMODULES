8130687ee06c92eea028049518b160f5bdbb4e09
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-14 03:46:59 -0400

    file with docstring

--- modulefunction.py
+++ modulefunction.py
@@ -3,11 +3,17 @@
 class Mathematics:
     """this is main class whose name is mathematics"""
     def __init__(self, num):
+        """
+        :param num: number
+        """
         self.num = num
         self.num_copy = num
 
     def factorial(self):
-        """this factorial function return the factorial of a number """
+        """
+        this factorial function return the factorial of a number
+        :return:number
+        """
         if self.num_copy != 0:
             self.num_copy -= 1
         if self.num_copy == 0:
@@ -15,15 +21,25 @@ class Mathematics:
         else:
             return (self.num_copy + 1) * self.factorial()
 
+    @property
     def square(self):
         """
-        this function return the square of
+         this function return the square of
         number that is entered in URL by user
-         """
+        :return: number
+        """
+
         return self.num * self.num
 
     def cube(self):
+        """
+        this cube function take number and return cube of number
+        :return: number
+        """
      return self.num * self.num * self.num
 
     def square_root(self):
+        """
+        :return:number
+        """
       return self.num ** 0.5
