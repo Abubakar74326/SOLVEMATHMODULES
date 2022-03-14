161bfa6bad4eacb38e40ff7daf2a054ad3ae5334
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-14 03:57:42 -0400

    modulefunction branch file with docstring

--- modulefunction.py
+++ modulefunction.py
@@ -1,13 +1,19 @@
-# Below code return the factorial,square,cube,square root of the number.
 
+# Below code return the factorial,square,cube,square root of the number.
 class Mathematics:
     """this is main class whose name is mathematics"""
     def __init__(self, num):
+        """
+        :param num: number
+        """
         self.num = num
         self.num_copy = num
 
     def factorial(self):
-        """this factorial function return the factorial of a number """
+        """
+        this factorial function return the factorial of a number
+        :return:number
+        """
         if self.num_copy != 0:
             self.num_copy -= 1
         if self.num_copy == 0:
@@ -15,19 +21,25 @@ class Mathematics:
         else:
             return (self.num_copy + 1) * self.factorial()
 
+    @property
     def square(self):
         """
-        this function return the square of
-        number that is enter in URL by user
-         """
+         this function return the square of
+        number that is entered in URL by user
+        :return: number
+        """
+
         return self.num * self.num
 
     def cube(self):
-
+        """
+        this cube function take number and return cube of number
+        :return: number
+           """
         return self.num * self.num * self.num
 
     def square_root(self):
+         """
+        :return:number
         """
-         this function return the square of a number
-                     """
-        return self.num ** 0.5
+         return self.num ** 0.5
