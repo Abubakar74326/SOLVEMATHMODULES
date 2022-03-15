53acbbfa9788d4f6ccd2a5fcc69e5337bd125387
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-15 03:04:19 -0400

    RUN this file

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 """modulefunction file is import in api file and api import in main file to run the project """
 import api
 if __name__ == "__main__":
-    api.app.run(host="0.0.0.0", port=5004, debug=True)
+    api.app.run(host="0.0.0.0, port=5004, debug=True)
